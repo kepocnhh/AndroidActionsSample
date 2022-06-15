5d59d5440366ac3a3fa46997f2330ec11234712a
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-15 22:47:05 +0300

    keystore fingerprints

--- buildSrc/src/main/resources/bash/project/prepare.sh
+++ buildSrc/src/main/resources/bash/project/prepare.sh
@@ -4,16 +4,24 @@ echo "Project prepare..."
 
 SCRIPTS=repository/buildSrc/src/main/resources/bash
 
-. $SCRIPTS/util/require BUILD_VARIANT KEYSTORE KEYSTORE_PASSWORD
+. $SCRIPTS/util/require BUILD_VARIANT KEYSTORE KEYSTORE_PASSWORD KEYSTORE_FINGERPRINT
 
-echo "$KEYSTORE" | base64 -d > repository/app/src/$BUILD_VARIANT/resources/key.pkcs12 \
+RESOURCES=repository/app/src/$BUILD_VARIANT/resources
+echo "$KEYSTORE" | base64 -d > $RESOURCES/key.pkcs12 \
  || . $SCRIPTS/util/throw 11 "Keystore error!"
 
 ESCAPED=${KEYSTORE_PASSWORD//"\\"/"\\\\"}
-echo "password=$ESCAPED" > repository/app/src/$BUILD_VARIANT/resources/properties \
+echo "password=$ESCAPED" > $RESOURCES/properties \
  || . $SCRIPTS/util/throw 12 "Keystore password error!"
 
+ACTUAL_FINGERPRINT="$(openssl pkcs12 -in $RESOURCES/key.pkcs12 -nokeys -passin pass:"$KEYSTORE_PASSWORD" \
+ | openssl x509 -noout -fingerprint -sha256)" \
+ || . $SCRIPTS/util/throw 13 "Actual fingerprint error!"
+
+test "$ACTUAL_FINGERPRINT" != "SHA256 Fingerprint=${KEYSTORE_FINGERPRINT}" \
+ && . $SCRIPTS/util/throw 14 "Expected fingerprint error!"
+
 gradle -p repository app:compile${BUILD_VARIANT}Sources \
- || . $SCRIPTS/util/throw 13 "Gradle compile error!"
+ || . $SCRIPTS/util/throw 15 "Gradle compile error!"
 
 exit 0
