9c220d191af75714ea83dadd611474cf46b451d9
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-17 15:47:37 +0300

    fix verify unit test

--- buildSrc/src/main/resources/bash/project/verify.sh
+++ buildSrc/src/main/resources/bash/project/verify.sh
@@ -27,13 +27,22 @@ CODE=0
 
 ENVIRONMENT=repository/buildSrc/src/main/resources/json/unit_test.json
 TYPE="UNIT_TEST"
-gradle -p repository "$(jq -Mcer ".${TYPE}.task" $ENVIRONMENT)"; CODE=$?
+TASK=$($SCRIPTS/util/jqx -sfs $ENVIRONMENT ".${TYPE}.task") \
+ || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+TASK=${TASK//"?"/"$BUILD_VARIANT"}
+gradle -p repository "$TASK"; CODE=$?
 if test $CODE -ne 0; then
  echo "Unit test error!"; exit 121
 else
  TYPE="TEST_COVERAGE"
- gradle -p repository "$(jq -Mcer ".${TYPE}.task" $ENVIRONMENT)" || exit 1 # todo
- gradle -p repository "$(jq -Mcer ".${TYPE}.verification.task" $ENVIRONMENT)" \
+ TASK=$($SCRIPTS/util/jqx -sfs $ENVIRONMENT ".${TYPE}.task") \
+  || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+ TASK=${TASK//"?"/"$BUILD_VARIANT"}
+ gradle -p repository "$TASK" || exit 1 # todo
+ TASK=$($SCRIPTS/util/jqx -sfs $ENVIRONMENT ".${TYPE}.verification.task") \
+  || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+ TASK=${TASK//"?"/"$BUILD_VARIANT"}
+ gradle -p repository "$TASK" \
   || . $SCRIPTS/util/throw 122 "Test coverage verification error!"
 fi
 
