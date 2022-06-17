73c53b026797bab65d391093ff7146c0922957b9
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-17 14:54:31 +0300

    fix unit test

--- app/src/test/kotlin/org/kepocnhh/aas/util/androidx/lifecycle/ViewModelUtil.kt
+++ app/src/test/kotlin/org/kepocnhh/aas/util/androidx/lifecycle/ViewModelUtil.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.runBlocking
 import org.kepocnhh.aas.foundation.provider.injection.Injection
 
-inline fun <reified T : ViewModel> Injection.viewModel(): T {
+internal inline fun <reified T : ViewModel> Injection.viewModel(): T {
     return T::class.java.getConstructor(Injection::class.java).newInstance(this)
 }
 
-inline fun <reified T : ViewModel> Injection.runViewModel(crossinline block: suspend (T) -> Unit) {
+internal inline fun <reified T : ViewModel> Injection.runViewModel(crossinline block: suspend (T) -> Unit) {
     runBlocking { block(viewModel()) }
 }
